904ed1e1da2ddd44ad5dc84a29b87cecab43d7bf
indspug <20477018+indspug@users.noreply.github.com>
2019-08-12 14:19:28 +0000

    RNNお試しソース2の入力を水戸,静岡,大阪,東京の天気に変更

--- rnn_sample2.py
+++ rnn_sample2.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 '''
   RNNの動作確認用2
-    24時間分の湿度,気圧,天気から、3時間後の天気を予測する
+    12時間分の天気から、3時間後の水戸の天気を予測する
 '''
 
 import sys, os
-import datetime
 from common.file import *
 from common.utility import *
 from common.processing import *
@@ -18,14 +17,14 @@ from keras.layers import Dropout
 from keras.layers.recurrent import LSTM
 from keras import optimizers
 
-LENGTH_OF_SEQUENCES = 24	# 過去24時間のデータから予測する
-LENGTH_OF_SHIFT = 6 		# 3時間後のデータを予測する
-NUMBER_OF_INPUT_NODES = 6	# 入力データ数(気温,相対湿度,海面気圧)
-NUMBER_OF_HIDDEN_NODES = 64	# 隠れ層のノード数
-NUMBER_OF_OUTPUT_NODES = 3	# 出力データ数(晴れ,曇り,雨
+LENGTH_OF_SEQUENCES = 12	# 過去12時間のデータから予測する
+LENGTH_OF_SHIFT = 3		# 3時間後のデータを予測する
+NUMBER_OF_INPUT_NODES = 4	# 入力データ数(水戸の天気,東京の天気,静岡の天気,大阪の天気)
+NUMBER_OF_HIDDEN_NODES = 32	# 隠れ層のノード数
+NUMBER_OF_OUTPUT_NODES = 3	# 出力データ数(晴れ,曇り,雨)
 SIZE_OF_BATCH = 128		# バッチサイズ
-DROPOUT_RATE = 0.5		# ドロップアウト率
-LEARNING_RATE = 0.001		# 学習率
+DROPOUT_RATE = 0.1		# ドロップアウト率
+LEARNING_RATE = 0.01		# 学習率
 NUMBER_OF_EPOCHS = 10		# 1回の学習のエポック数
 NUMBER_OF_TRAINING = 50		# 学習回数
 RESULT_FILE_WHOLE = './result/rnn_result_190812_02_whole.csv'
@@ -47,20 +46,22 @@ def load_data(dir_path, point_name):
 		csv_data = read_weather_csv(csv_path)
 		#temp = get_temperature(csv_data, point_name)
 		#temperature = numpy.append(temperature, temp)
-		humi = get_humidity(csv_data, point_name)
-		humidity = numpy.append(humidity, humi)
-		pres = get_sea_level_pressure(csv_data, point_name)
-		pressure = numpy.append(pressure, pres)
+		#humi = get_humidity(csv_data, point_name)
+		#humidity = numpy.append(humidity, humi)
+		#pres = get_sea_level_pressure(csv_data, point_name)
+		#pressure = numpy.append(pressure, pres)
 		weat_v = get_variable_weather(csv_data, point_name)
 		weather_value = numpy.append(weather_value, weat_v)
 		weat_l = get_weather(csv_data, point_name)
 		weather_label = numpy.append(weather_label, weat_l)
 		
 	#re_input = numpy.stack([temperature, humidity, pressure, weather_value], 1)
-	re_input = numpy.stack([humidity, pressure, weather_value], 1)
+	#re_input = numpy.stack([humidity, pressure, weather_value], 1)
+	re_input = numpy.stack([weather_value], 1)
 	re_target = numpy.reshape(weather_label,
 			(weather_label.shape[0]/WEATHER_CLASS_NUM, WEATHER_CLASS_NUM))
 	
+	#print(re_input.shape)
 	re_input_interpolated  = interpolate_nan_input_data(re_input)
 	re_target_interpolated = interpolate_nan_label_data(re_target)
 	
@@ -93,7 +94,7 @@ def output_whole_result_header():
 	
 	fo = open(RESULT_FILE_WHOLE, 'a')
 	fo.write('##################################################\n')
-	fo.write('入力データ = 相対湿度 海面気圧 天気\n')
+	fo.write('入力データ = 水戸の天気,東京の天気,静岡の天気,大阪の天気\n')
 	fo.write('model = %d x LSTM(%dx%d)x DropOut(%d) x %d\n'
 		% (NUMBER_OF_INPUT_NODES, NUMBER_OF_INPUT_NODES,
 		   NUMBER_OF_HIDDEN_NODES, NUMBER_OF_OUTPUT_NODES, NUMBER_OF_OUTPUT_NODES) )
@@ -112,7 +113,7 @@ def output_result(input, target, predicted, number):
 	# 正解と予想結果をファイル出力
 	filename = str.format('%s_%03d.csv' % (RESULT_FILE_NAME, number) )
 	fo = open(filename, 'w')
-	fo.write('湿度(水戸),気圧(水戸),天気(水戸),湿度(大阪),気圧(大阪),天気(大阪),晴れ,曇り,雨,晴れ(予測),曇り(予測),雨(予測),正解/不正解\n')
+	fo.write('天気(水戸),天気(東京),静岡の天気,天気(大阪),晴れ,曇り,雨,晴れ(予測),曇り(予測),雨(予測),正解/不正解\n')
 	
 	# 全テストデータの正解と予想結果出力
 	data_len = input.shape[0]
@@ -124,21 +125,23 @@ def output_result(input, target, predicted, number):
 		
 		if i < (LENGTH_OF_SEQUENCES+LENGTH_OF_SHIFT):
 			# 予想結果が出せないデータの場合(最初の方)
-			fo.write('%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
-				(input_i[0], input_i[1], input_i[2], 
-				 input_i[3], input_i[4], input_i[5],
+			fo.write('%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
+				(input_i[0], input_i[1], input_i[2], input_i[3],
+				 #input_i[3], input_i[4], input_i[5],
 				 target_i[0], target_i[1], target_i[2] ) )
 		else:
 			pi = i - LENGTH_OF_SEQUENCES - LENGTH_OF_SHIFT
 			
 			# 正解/不正解を確認する
-			correct_i = numpy.argmax(target)
+			correct_i = numpy.argmax(target_i)
 			predict_i = numpy.argmax(predicted[pi])
 			correct = 1 if (correct_i == predict_i) else 0
+			#print("%.2f,%.2f,%.2f" % (target, predict_i, correct) )
+			#print("%d,%d,%d" % (correct_i, predict_i, correct) )
 			
-			fo.write('%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n' % 
-				(input_i[0], input_i[1], input_i[2],
-				 input_i[3], input_i[4], input_i[5],
+			fo.write('%f,%f,%f,%f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n' % 
+				(input_i[0], input_i[1], input_i[2], input_i[3],
+				 #input_i[3], input_i[4], input_i[5],
 				 target_i[0], target_i[1], target_i[2],
 				 predicted[pi,0], predicted[pi,1], predicted[pi,2],
 				 correct) )
@@ -151,23 +154,24 @@ def output_result(input, target, predicted, number):
 if __name__ == '__main__':
 	
 	# 学習用データ取得
-	train_input_raw1, train_target_raw1 = load_data('./train/mito',  '水戸')
-	train_input_raw2, train_target_raw2 = load_data('./train/osaka', '大阪')
-	train_input_raw = numpy.hstack( [train_input_raw1, train_input_raw2] )
+	train_input_raw1, train_target_raw1 = load_data('./train/mito',     '水戸')
+	train_input_raw2, train_target_raw2 = load_data('./train/tokyo',    '東京')
+	train_input_raw3, train_target_raw3 = load_data('./train/shizuoka', '静岡')
+	train_input_raw4, train_target_raw4 = load_data('./train/osaka',    '大阪')
+	train_input_raw = numpy.hstack(
+				[train_input_raw1, train_input_raw2, 
+				 train_input_raw3, train_input_raw4] )
 	train_target_raw = train_target_raw1
 	
 	# テスト用データ取得
-	test_input_raw1, test_target_raw1 = load_data('./test/mito',  '水戸')
-	test_input_raw2, test_target_raw2 = load_data('./test/osaka', '大阪')
-	test_input_raw = numpy.hstack( [test_input_raw1, test_input_raw2] )
+	test_input_raw1, test_target_raw1 = load_data('./test/mito',     '水戸')
+	test_input_raw2, test_target_raw2 = load_data('./test/tokyo',    '東京')
+	test_input_raw3, test_target_raw3 = load_data('./test/shizuoka', '静岡')
+	test_input_raw4, test_target_raw4 = load_data('./test/osaka',    '大阪')
+	test_input_raw = numpy.hstack(
+				[test_input_raw1, test_input_raw2, 
+				 test_input_raw3, test_input_raw4] )
 	test_target_raw = test_target_raw1
-	
-	print(train_input_raw1.shape)
-	print(train_input_raw2.shape)
-	print(train_input_raw.shape)
-	print(test_input_raw1.shape)
-	print(test_input_raw2.shape)
-	print(test_input_raw.shape)
 	
 	# Max-Minスケール化
 	scaler, train_input_scaled, test_input_scaled = \
@@ -179,8 +183,9 @@ if __name__ == '__main__':
 	
 	# モデル作成
 	model = Sequential()
-	model.add( LSTM( NUMBER_OF_HIDDEN_NODES, 
+	model.add( LSTM(NUMBER_OF_HIDDEN_NODES, 
 			input_shape=(LENGTH_OF_SEQUENCES, NUMBER_OF_INPUT_NODES), 
+			dropout=DROPOUT_RATE, recurrent_dropout=DROPOUT_RATE,
 			return_sequences=False) )
 	model.add(Dense(NUMBER_OF_OUTPUT_NODES))
 	model.add(Dropout(DROPOUT_RATE))
