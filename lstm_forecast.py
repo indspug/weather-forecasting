0599ea268d0c0b5bbb6d2d9ec2c07d9e3cf8330c
indspug <20477018+indspug@users.noreply.github.com>
2019-08-12 17:53:42 +0000

    入力を[水戸,前橋,東京,静岡,大阪]の天気に変更(再)

--- lstm_forecast.py
+++ lstm_forecast.py
@@ -19,17 +19,17 @@ from keras import optimizers
 
 LENGTH_OF_SEQUENCES = 12	# 過去12時間のデータから予測する
 LENGTH_OF_SHIFT = 4		# 4時間後のデータを予測する
-NUMBER_OF_INPUT_NODES = 12	# 入力データ数:(水戸,前橋,東京)x(降水量,湿度,気圧,天気)
-NUMBER_OF_HIDDEN_NODES = 64	# 隠れ層のノード数
+NUMBER_OF_INPUT_NODES = 5   	# 入力データ数:(水戸,前橋,東京)x(降水量,湿度,気圧,天気)
+NUMBER_OF_HIDDEN_NODES = 32	# 隠れ層のノード数
 NUMBER_OF_OUTPUT_NODES = 3	# 出力データ数(晴れ,曇り,雨)
 SIZE_OF_BATCH = 128		# バッチサイズ
 DROPOUT_RATE = 0.1		# ドロップアウト率
-LEARNING_RATE = 0.05		# 学習率
+LEARNING_RATE = 0.02		# 学習率
 NUMBER_OF_EPOCHS = 10		# 1回の学習のエポック数
 NUMBER_OF_TRAINING = 50		# 学習回数
 OUTPUT_CYCLE = 1		# 学習経過出力周期
-RESULT_FILE_WHOLE = './result/rnn_result_190812_05_whole.csv'
-RESULT_FILE_NAME  = './result/rnn_result_190812_05'
+RESULT_FILE_WHOLE = './result/rnn_result_190812_06_whole.csv'
+RESULT_FILE_NAME  = './result/rnn_result_190812_06'
 
 ##################################################
 # 学習用データ取得
@@ -48,18 +48,19 @@ def load_data(dir_path, point_name):
 		csv_data = read_weather_csv(csv_path)
 		#temp = get_temperature(csv_data, point_name)
 		#temperature = numpy.append(temperature, temp)
-		rain = get_rainfall(csv_data, point_name)
-		rainfall = numpy.append(rainfall, rain)
-		humi = get_humidity(csv_data, point_name)
-		humidity = numpy.append(humidity, humi)
-		pres = get_sea_level_pressure(csv_data, point_name)
-		pressure = numpy.append(pressure, pres)
+		#rain = get_rainfall(csv_data, point_name)
+		#rainfall = numpy.append(rainfall, rain)
+		#humi = get_humidity(csv_data, point_name)
+		#humidity = numpy.append(humidity, humi)
+		#pres = get_sea_level_pressure(csv_data, point_name)
+		#pressure = numpy.append(pressure, pres)
 		weat_v = get_variable_weather(csv_data, point_name)
 		weather_value = numpy.append(weather_value, weat_v)
 		weat_l = get_weather(csv_data, point_name)
 		weather_label = numpy.append(weather_label, weat_l)
 		
-	re_input = numpy.stack([rainfall, humidity, pressure, weather_value], 1)
+	#re_input = numpy.stack([rainfall, humidity, pressure, weather_value], 1)
+	re_input = numpy.stack([weather_value], 1)
 	re_target = numpy.reshape(weather_label,
 			(weather_label.shape[0]/WEATHER_CLASS_NUM, WEATHER_CLASS_NUM))
 	
@@ -95,12 +96,15 @@ def output_whole_result_header():
 	
 	fo = open(RESULT_FILE_WHOLE, 'a')
 	fo.write('##################################################\n')
-	fo.write('入力データ = (水戸,前橋,東京)x(降水量,湿度,気圧,天気)\n')
+	fo.write('入力データ = 水戸,前橋,東京,静岡,大阪の天気\n')
+	#fo.write('入力データ = (水戸,前橋,東京)x(降水量,湿度,気圧,天気)\n')
 	fo.write('model = %d x LSTM(%d x %d) x DropOut(%d) x %d\n'
 		% (NUMBER_OF_INPUT_NODES, NUMBER_OF_INPUT_NODES,
 		   NUMBER_OF_HIDDEN_NODES, NUMBER_OF_OUTPUT_NODES, NUMBER_OF_OUTPUT_NODES) )
 	fo.write('dropout rate = %f\n' % (DROPOUT_RATE) )
 	fo.write('optimizer = Adam(lr=%f)\n' % (LEARNING_RATE) )
+	fo.write('length of sequence = %d\n' % (LENGTH_OF_SEQUENCES) )
+	fo.write('length of shift = %d\n' % (LENGTH_OF_SHIFT) )
 	fo.write('date: ' + get_datetime_string() + '\n')
 	fo.write('##################################################\n')
 	fo.write('epoch,loss acc\n')
@@ -114,8 +118,10 @@ def output_result(input, target, predicted, number):
 	# 正解と予想結果をファイル出力
 	filename = str.format('%s_%03d.csv' % (RESULT_FILE_NAME, number) )
 	fo = open(filename, 'w')
-	fo.write('水戸,,,,前橋,,,,東京,,,,水戸(正解),,,水戸(予測),,,,\n')
-	fo.write('降水量,湿度,気圧,天気,降水量,湿度,気圧,天気,降水量,湿度,気圧,天気,晴れ,曇り,雨,晴れ,曇り,雨,正解/不正解\n')
+	#fo.write('水戸,,,,前橋,,,,東京,,,,水戸(正解),,,水戸(予測),,,,\n')
+	#fo.write('降水量,湿度,気圧,天気,降水量,湿度,気圧,天気,降水量,湿度,気圧,天気,晴れ,曇り,雨,晴れ,曇り,雨,正解/不正解\n')
+	fo.write('水戸,前橋,東京,静岡,大阪,水戸(正解),,,水戸(予測),,,,\n')
+	fo.write('天気,天気,天気,天気,天気,晴れ,曇り,雨,晴れ,曇り,雨,正解/不正解\n')
 	
 	# 全テストデータの正解と予想結果出力
 	data_len = input.shape[0]
@@ -127,12 +133,14 @@ def output_result(input, target, predicted, number):
 		
 		if i < (LENGTH_OF_SEQUENCES+LENGTH_OF_SHIFT):
 			# 予想結果が出せないデータの場合(最初の方)
-			#fo.write('%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
-			fo.write('%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
-				(input_i[0], input_i[1], input_i[2], input_i[3],
-				 input_i[4], input_i[5], input_i[6], input_i[7],
-				 input_i[8], input_i[9], input_i[10], input_i[11],
+			fo.write('%f,%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
+				(input_i[0], input_i[1], input_i[2], input_i[3], input_i[4],
 				 target_i[0], target_i[1], target_i[2] ) )
+			#fo.write('%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f\n' %
+			#	(input_i[0], input_i[1], input_i[2], input_i[3],
+			#	 input_i[4], input_i[5], input_i[6], input_i[7],
+			#	 input_i[8], input_i[9], input_i[10], input_i[11],
+			#	 target_i[0], target_i[1], target_i[2] ) )
 		else:
 			pi = i - LENGTH_OF_SEQUENCES - LENGTH_OF_SHIFT
 			
@@ -141,13 +149,17 @@ def output_result(input, target, predicted, number):
 			predict_i = numpy.argmax(predicted[pi])
 			correct = 1 if (correct_i == predict_i) else 0
 			
-			fo.write('%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n' % 
-				(input_i[0], input_i[1], input_i[2], input_i[3],
-				 input_i[4], input_i[5], input_i[6], input_i[7],
-				 input_i[8], input_i[9], input_i[10], input_i[11],
+			fo.write('%f,%f,%f,%f,%f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n' % 
+				(input_i[0], input_i[1], input_i[2], input_i[3], input_i[4],
 				 target_i[0], target_i[1], target_i[2],
-				 predicted[pi,0], predicted[pi,1], predicted[pi,2],
-				 correct) )
+				 predicted[pi,0], predicted[pi,1], predicted[pi,2], correct ) )
+			#fo.write('%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d\n' % 
+			#	(input_i[0], input_i[1], input_i[2], input_i[3],
+			#	 input_i[4], input_i[5], input_i[6], input_i[7],
+			#	 input_i[8], input_i[9], input_i[10], input_i[11],
+			#	 target_i[0], target_i[1], target_i[2],
+			#	 predicted[pi,0], predicted[pi,1], predicted[pi,2],
+			#	 correct) )
 				
 	fo.close()
 
@@ -160,11 +172,11 @@ if __name__ == '__main__':
 	train_input_raw1, train_target_raw1 = load_data('./train/mito',     '水戸')
 	train_input_raw2, train_target_raw2 = load_data('./train/maebashi', '前橋')
 	train_input_raw3, train_target_raw3 = load_data('./train/tokyo',    '東京')
-	#train_input_raw4, train_target_raw4 = load_data('./train/shizuoka', '静岡')
-	#train_input_raw5, train_target_raw5 = load_data('./train/osaka',    '大阪')
+	train_input_raw4, train_target_raw4 = load_data('./train/shizuoka', '静岡')
+	train_input_raw5, train_target_raw5 = load_data('./train/osaka',    '大阪')
 	train_input_raw = numpy.hstack(
-				[train_input_raw1, train_input_raw2, train_input_raw3] )
-				 #train_input_raw3, train_input_raw4, train_input_raw5] )
+				[train_input_raw1, train_input_raw2, train_input_raw3,
+				 train_input_raw4, train_input_raw5] )
 	#train_input_raw = train_input_raw1
 	train_target_raw = train_target_raw1
 	
@@ -172,11 +184,11 @@ if __name__ == '__main__':
 	test_input_raw1, test_target_raw1 = load_data('./test/mito',     '水戸')
 	test_input_raw2, test_target_raw2 = load_data('./test/maebashi', '前橋')
 	test_input_raw3, test_target_raw3 = load_data('./test/tokyo',    '東京')
-	#test_input_raw4, test_target_raw4 = load_data('./test/shizuoka', '静岡')
-	#test_input_raw5, test_target_raw5 = load_data('./test/osaka',    '大阪')
+	test_input_raw4, test_target_raw4 = load_data('./test/shizuoka', '静岡')
+	test_input_raw5, test_target_raw5 = load_data('./test/osaka',    '大阪')
 	test_input_raw = numpy.hstack(
-				[test_input_raw1, test_input_raw2, test_input_raw3] )
-				 #test_input_raw3, test_input_raw4, test_input_raw5] )
+				[test_input_raw1, test_input_raw2, test_input_raw3,
+				 test_input_raw4, test_input_raw5] )
 	#test_input_raw = test_input_raw1
 	test_target_raw = test_target_raw1
 	
